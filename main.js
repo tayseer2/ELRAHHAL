80710c590744e219a430183a49b5f92d38fa57f0
tayseer alrahhal <143759119+tayseer2@users.noreply.github.com>
2024-05-13 00:00:00 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+Popup 
+let popDiv = document.querySelector(".popup");
+let popBtn = document.querySelector(".btn");
+setTimeout(shwoPopUP, 5000)
+function shwoPopUP() {
+    popDiv.style.display = "block"
+}
+
+popBtn.onclick = function () {
+    popDiv.remove()
+}
+
+// Scroll To Top
+let btn = document.querySelector(".scrole-to-top");
+window.onscroll = function () {
+    if (window.scrollY >= 650) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+}
+window.onload = function () {
+    btn.style.display = "none";
+}
+btn.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
